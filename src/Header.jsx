9d22fc79744cf5bc037b7f8c0f571b2634b8e0a9
julio alberto perez <piyoyo279@gmail.com>
2023-10-23 19:27:51 -0400

    pruebadeconceptos

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,14 +3,23 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import './header.css';
-
+import { useNavigate } from 'react-router-dom';
 const Header=()=>{
-
+  const navigate = useNavigate();
   const Redireccion = () => {
     window.location.href = 'https://nodejsprueba.onrender.com/api/v1/docs/';
    
  };
- 
+ const servidor = () => {
+  const n=navigate('/servidor');
+  return n;
+    
+  };
+  const guia = () => {
+    const n=navigate('/guia');
+    return n;
+      
+    };
 
 return(<>
  <Navbar expand="lg" className="bg-body-tertiary">
@@ -24,8 +33,8 @@ return(<>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/"> <i className="fa-solid fa-file"></i>AppArchivo</Nav.Link>
-            <Nav.Link href="/servidor"> <i className="fa-solid fa-server"></i>Servidor</Nav.Link>
-            <Nav.Link href="/guia"> <i className="fa-solid fa-guarani-sign"></i>Guia</Nav.Link>
+            <Nav.Link href="#servidor" onClick={servidor}><i className="fa-solid fa-server"></i>Servidor</Nav.Link>
+            <Nav.Link href="#guia" onClick={guia }> <i className="fa-solid fa-guarani-sign"></i>Guia</Nav.Link>
             <Nav.Link href="#docuemtnacion" onClick={Redireccion }> <i className="fa-solid fa-folder"></i>Documentacion</Nav.Link>
            
           </Nav>
